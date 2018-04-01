3cce74eb295b49604f5c212d68c273565b43dd32
Syed Aman <sna274@nyu.edu>
2018-04-01 00:14:40 -0500

    delete old stuff

--- src/add-thousand-selector.js
+++
@@ -1,9 +0,0 @@
-module.exports = number => {
-    const arrayOfNumbers = number.split('');
-    
-    for (let position = arrayOfNumbers.length; position > 3; position -= 3) {
-        arrayOfNumbers.splice(position - 3, 0, ',');
-    }
-
-    return arrayOfNumbers.join('');
-}
--- src/add-thousand-selector.spec.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-
-const addThousandSelector = require('./add-thousand-selector');
-
-describe('The addThousandSelector function', () => {
-    it('should work', () => {
-        expect(addThousandSelector("5000")).to.equal("5,000");
-    })
-})
